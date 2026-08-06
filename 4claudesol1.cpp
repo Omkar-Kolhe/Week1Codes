7c5e2f8f287c8c758ec2c0908220ed93ce3012b8
Tanvir Harpreet Singh Kohli <kohlitanvirsingh@gmail.com>
2026-08-06 11:43:43 +0530

    4claudesol1.cpp

---
+++ 4claudesol1.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    
+    int n;
+    cin >> n;
+    
+    vector<long long> a(n);
+    long long sum = 0;
+    for (int i = 0; i < n; i++) {
+        cin >> a[i];
+        sum += a[i];
+    }
+    
+    cout << (sum % n == 0 ? "YES" : "NO") << "\n";
+    return 0;
+}
